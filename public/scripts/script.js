d5b7b11d18bf5ad8945131e119eded6d41c219c4
Ryan da Silva de Castro Pereira <ryandasilva1008@gmail.com>
2022-11-23 16:12:09 -0300

    parte visual do front

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,11 +2,28 @@ document.addEventListener('DOMContentLoaded', ()=>{
     updatePromotions()
 })
 
+let gamesOnServer
+
+function $(element){
+    return document.querySelector(element)
+}
+
 function updatePromotions(){
 
     fetch('http://localhost:3000/api/promotions/2019').then(res=>{
         return res.json()
     }).then(json => {
-        console.log(json)
+        
     })
+}
+
+function showNavBar(){
+    $("nav").style.marginLeft = "0vw"
+    $("#layerOfNavBar").style.visibility = "visible"
+}
+
+function hideNavBar(){
+    $("nav").style.marginLeft = "-41vw"
+    $("#layerOfNavBar").style.visibility = "hidden"
+
 }
