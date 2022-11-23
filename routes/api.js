02257f98208371c7e76ac3ce1377b086d7c04771
Ryan da Silva de Castro Pereira <ryandasilva1008@gmail.com>
2022-11-22 22:14:12 -0300

    tentativa de fazer o bot

--- routes/api.js
+++ routes/api.js
@@ -1,62 +1,44 @@
 const express = require('express');
 const router = express.Router()
 const puppeteer = require('puppeteer');
+const fs = require("fs")
 
 async function getPromotions(){
     
 
-    async function loadMore(page,selector){
-        const moreButton = await page.$(selector)
-        if(moreButton){
-            
-            console.log("more")
-            await moreButton.click()
-            await page.waitForSelector(selector, {timeout: 5000}).catch(()=>{console.log("timeout")})
-            await loadMore(page, selector)
-        }
-    }
-
-    async function autoScroll(page){
-        await page.evaluate(async () => {
-            await new Promise((resolve) => {
-                var totalHeight = 0;
-                var distance = 100;
-                var timer = setInterval(() => {
-                    var scrollHeight = document.body.scrollHeight;
-                    window.scrollBy(0, distance);
-                    totalHeight += distance;
-    
-                    if(totalHeight >= scrollHeight - window.innerHeight){
-                        clearInterval(timer);
-                        resolve();
-                    }
-                }, 100);
-            });
-        });
-    }
-    
-
-    async function getgame(page, selector){
+    async function getgames(page, selector){
         const promotions = await page.$$eval(selector, links => links.map(link => link.innerText))
         return promotions
     }
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://store.steampowered.com/specials');
-    await autoScroll(page)
+    await page.goto('https://jogorama.com.br/jogos/lancados-em-2022/');
 
-    await loadMore(page, ".saleitembrowser_ShowContentsButton_3d9cK")
-    const promotions = await getgame(page, '.salepreviewwidgets_SaleItemBrowserRow_y9MSd .salepreviewwidgets_TitleCtn_1F4bc a')
-    
+    const games = await getgames(page, '#colunacentral .lista a')
+    let itens
+    games.forEach((game)=>{
+        fs.readFile("./files/gamesOf2022.txt", 'UTF8', async (error, data)=>{
+            if(error){throw error}
+            itens =  await JSON.parse(data);
+            if(itens[game]){
+                
+            } else {
+                itens[game] = "{}"
+                fs.writeFile("./files/gamesOf2022.txt", JSON.stringify(itens), (error)=>{
+                    if(error){throw error}
+                })
+            }
+        })
+    })
 
     await browser.close();
-    return promotions
+    return itens
 }
 
-router.get("/promotions", (req, res)=>{
+router.get("/promotions", async(req, res)=>{
 
-    res.send(getPromotions())
+    res.send(await getPromotions())
 })
 
 module.exports = router
